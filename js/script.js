668f4441db327fb8ed0f8e7a14ce7febf82946c5
dabadeedabadae <positionself@gmail.com>
2024-09-17 11:33:13 +0500

    project

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 document.getElementById("burger2").addEventListener("click", function() { 
-    document.querySelector ("-navbar").classList.toggle("open")
+    document.querySelector(".navbar").classList.toggle("open")
 })
